2cb2038bb1c2371087e7f29953c3cf20eaaf578b
iggy228 <i.borovy@gmail.com>
2021-07-07 15:05:40 +0200

    Making navbar with gradient body background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Navbar from './components/Navbar'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Ignac Borovy</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About me</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/contact">Contact me</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
